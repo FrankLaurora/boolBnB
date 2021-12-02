3fb8f7628023229b5b5422cd1d5f2069c868a373
FrankLaurora <francesco.laurora9@gmail.com>
2021-12-02 16:45:34 +0100

    Add migrations, error on migrating pivot

---
+++ database/migrations/2021_12_02_151229_create_apartment_sponsorship_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApartmentSponsorshipTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('apartment_sponsorship', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('apartment_id')->constrained()->onDelete('cascade');
+            // $table->foreignId('sponsorship_id')->constrained('sponsorships')->onDelete('set null');
+            $table->foreign('sponsorship_id')->references('id')->on('sponsorships')->onDelete('cascade');
+            $table->dateTime('expiration');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('apartment_sponsorship');
+    }
+}
