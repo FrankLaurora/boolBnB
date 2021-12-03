1bc06f67103686498e7d8ed18cbf0e7ac0e40630
FrankLaurora <francesco.laurora9@gmail.com>
2021-12-03 12:58:35 +0100

    Apartments create in progress

--- resources/views/Admin/Apartments/create.blade.php
+++ resources/views/Admin/Apartments/create.blade.php
@@ -7,21 +7,115 @@
         <title>New Apartment</title>
     </head>
     <body>
-        <form>
-            <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        <form action="{{route("admin.apartments.store")}}" method="POST" enctype="multipart/form-data">
+            @csrf
+            {{-- input per il titolo dell'appartamento --}}
+            <div class="mb-3">
+                <label for="title" class="form-label">Titolo</label>
+                <input type="text" name="title" class="form-control @error('title') is-invalid @enderror" id="title" value="{{old('title')}}" placeholder="Inserisci il titolo">
+                @error('title')
+                    <div class="alert alert-danger">{{$message}}</div>
+                @enderror
             </div>
-            <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+
+            {{-- input per il numero di stanze dell'appartamento --}}
+            <div class="mb-3">
+                <label for="rooms" class="form-label">Stanze</label>
+                <input type="number" name="rooms" class="form-control @error('rooms') is-invalid @enderror" id="rooms" value="{{old('rooms')}}" placeholder="Seleziona il numero di stanze">
+                @error('rooms')
+                    <div class="alert alert-danger">{{$message}}</div>
+                @enderror
             </div>
-            <div class="form-check">
-            <input type="checkbox" class="form-check-input" id="exampleCheck1">
-            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+
+            {{-- input per posti letto dell'appartamento --}}
+            <div class="mb-3">
+                <label for="guests_number" class="form-label">Posti letto</label>
+                <input type="number" name="guests_number" class="form-control @error('guests_number') is-invalid @enderror" id="guests_number" value="{{old('guests_number')}}" placeholder="Posti letto">
+                @error('guests_number')
+                    <div class="alert alert-danger">{{$message}}</div>
+                @enderror
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+
+            {{-- input per il numero di bagni dell'appartamento --}}
+            <div class="mb-3">
+                <label for="bathrooms" class="form-label">Bagni</label>
+                <input type="number" name="bathrooms" class="form-control @error('bathrooms') is-invalid @enderror" id="bathrooms" value="{{old('bathrooms')}}" placeholder="Bagni">
+                @error('bathrooms')
+                    <div class="alert alert-danger">{{$message}}</div>
+                @enderror
+            </div>
+
+            {{-- input per l'estensione dell'appartamento --}}
+            <div class="mb-3">
+                <label for="sqm" class="form-label">Metri quadrati</label>
+                <input type="number" name="sqm" class="form-control @error('sqm') is-invalid @enderror" id="sqm" value="{{old('sqm')}}" placeholder="Seleziona l'estensione">
+                @error('sqm')
+                    <div class="alert alert-danger">{{$message}}</div>
+                @enderror
+            </div>
+
+            {{-- input per la nazione dell'appartamento --}}
+            <div class="mb-3">
+                <label for="region" class="form-label">Stato</label>
+                <input type="text" name="region" class="form-control @error('region') is-invalid @enderror" id="region" value="{{old('region')}}" placeholder="Inserisci lo stato">
+                @error('region')
+                    <div class="alert alert-danger">{{$message}}</div>
+                @enderror
+            </div>
+
+            {{-- input per la città dell'appartamento --}}
+            <div class="mb-3">
+                <label for="city" class="form-label">Città</label>
+                <input type="text" name="city" class="form-control @error('city') is-invalid @enderror" id="city" value="{{old('city')}}" placeholder="Inserisci la città">
+                @error('city')
+                    <div class="alert alert-danger">{{$message}}</div>
+                @enderror
+            </div>
+
+            {{-- input per l'indirizzo dell'appartamento --}}
+            <div class="mb-3">
+                <label for="address" class="form-label">Indirizzo</label>
+                <input type="text" name="address" class="form-control @error('address') is-invalid @enderror" id="address" value="{{old('address')}}" placeholder="Inserisci l'indirizzo">
+                @error('address')
+                    <div class="alert alert-danger">{{$message}}</div>
+                @enderror
+            </div>
+
+            {{-- input per il numero civico dell'appartamento --}}
+            <div class="mb-3">
+                <label for="number" class="form-label">Numero civico</label>
+                <input type="number" name="number" class="form-control @error('number') is-invalid @enderror" id="number" value="{{old('number')}}" placeholder="Numero civico">
+                @error('number')
+                    <div class="alert alert-danger">{{$message}}</div>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <label for="title" class="form-label">Titolo</label>
+                <input type="text" name="title" class="form-control @error('title') is-invalid @enderror" id="title" value="{{old('title')}}" placeholder="Inserisci il titolo">
+                @error('title')
+                    <div class="alert alert-danger">{{$message}}</div>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <label for="content" class="form-label">Post</label>
+                <textarea name="content" id="content" class="form-control @error('content') is-invalid @enderror" cols="30" rows="10" placeholder="Inserisci il testo del tuo post... ">{{old('content')}}</textarea>
+                @error('content')
+                    <div class="alert alert-danger">{{$message}}</div>
+                @enderror
+            </div>
+            <div class="mb-3">
+                <label for="category" class="form-label">Categoria</label>
+                <select name="category_id" class="form-control @error('category_id') is-invalid @enderror" >
+                    <option value="">--seleziona una categoria--</option>
+                    @foreach ($categories as $category)
+                       <option {{ old("category_id") == $category['id'] ? 'selected' : null}} value="{{$category['id']}}">{{$category['name']}}</option> 
+                    @endforeach
+                </select>
+                @error('category_id')
+                    <div class="alert alert-danger">{{$message}}</div>
+                @enderror
+            </div>
+            <button type="submit" class="btn btn-dark">Pubblica</button>
         </form>
     </body>
 </html>
