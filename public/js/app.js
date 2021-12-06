5a55c512f0ce4b75eeedcd77b7420004903a1d57
alessandroNessi <alessandronessi@msn.com>
2021-12-06 13:09:18 +0100

    fix localization

--- public/js/app.js
+++ public/js/app.js
@@ -49931,8 +49931,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\MAMP\htdocs\php\boolBnB\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\MAMP\htdocs\php\boolBnB\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\aless\Desktop\CORSO BOOLEAN\progetto finale\boolBnB\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\aless\Desktop\CORSO BOOLEAN\progetto finale\boolBnB\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
