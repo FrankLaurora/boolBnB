f8e603f4b9653462ab8106e520e5c3671578c6a5
valentina cantarelli <valentinacanterina@gmail.com>
2021-12-07 17:29:50 +0100

    agiunta table messaggi

--- resources/views/Admin/Apartments/show.blade.php
+++ resources/views/Admin/Apartments/show.blade.php
@@ -18,7 +18,28 @@
            
        </ul>
        <div>Sponsorizza</div>
+       <div>
+           <button type="button" class="btn btn-secondary">Silver</button>
+           <button type="button" class="btn btn-warning">Gold</button>
+           <button type="button" class="btn btn-light">Platinum</button>
+        </div>
        <div>Messaggi ricevuti</div>
+       <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">Mittente</th>
+            <th scope="col">Messaggio</th>
+            <th scope="col">Data</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>Franco</td>
+            <td>Ciao!Bello l'appartamento, è disponibile il bla bla bla?</td>
+            <td>07/12/2021</td>
+          </tr>
+        </tbody>
+      </table>
        <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#deleteModal">
         Elimina annuncio
     </button>
