2dea51be9d059fcf46bb90591c84684ceaddfb34
FrankLaurora <francesco.laurora9@gmail.com>
2021-12-09 17:43:38 +0100

    Add imeges and services to apartments json

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -6,12 +6,48 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Apartment;
 use Dotenv\Result\Success;
+use App\Service;
 
 class ApartmentController extends Controller
 {
     public function index()
     {
         $apartments = Apartment::all();
+
+        
+        foreach($apartments as $apartment) {
+            $services = [];
+
+            foreach($apartment->services()->get() as $apartment_service){
+                $services[] = $apartment_service->id;
+            }
+
+            $apartment->services = $services;
+        };
+
+        foreach($apartments as $apartment) {
+            $images = [];
+
+            foreach($apartment->images()->get() as $apartment_image){
+                $images[] = $apartment_image->id;
+            }
+
+            $apartment->images = $images;
+        };
+
+        // die();
+
+        
+        // foreach($apartments as $apartment) {
+        //     $sponsorships = [];
+
+        //     foreach($apartment->sponsorships()->get() as $apartment_sponsorship){
+        //         if($apartment_sponsorship->created_at )
+        //         $sponsorships[] = $apartment_sponsorship->created_at;
+        //     }
+
+        //     $apartment->sponsorships = $sponsorships;
+        // };
 
         return response()->json([           
             'success' => true,
