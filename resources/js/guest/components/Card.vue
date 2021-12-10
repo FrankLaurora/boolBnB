c7975482416d7e8879639cec6188ef32ccd3406b
leonardobarone <leonardobarone26@gmail.com>
2021-12-10 12:22:54 +0100

    init card

---
+++ resources/js/guest/components/Card.vue
@@ -0,0 +1,150 @@
+<template> 
+     
+     <div class="container">
+
+         <div class="col">
+            <div class="card">
+                <div class="card-head">
+                    <img src="../images/prova.jpg" alt="">
+                </div>
+                <div class="card-body">
+                    <h3>Happy Days Garden</h3>
+                    <div>Procida, 80079</div>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos aliquam magnam tenetur, explicabo ipsa officia sed aliquid quia nihil qui provident dicta cupiditate inventore aspernatur, corrupti alias eligendi obcaecati id?</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card">
+                <div class="card-head">
+                    <img src="../images/prova.jpg" alt="">
+                </div>
+                <div class="card-body">
+                    <h3>Happy Days Garden</h3>
+                    <div>Procida, 80079</div>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos aliquam magnam tenetur, explicabo ipsa officia sed aliquid quia nihil qui provident dicta cupiditate inventore aspernatur, corrupti alias eligendi obcaecati id?</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card">
+                <div class="card-head">
+                    <img src="../images/prova.jpg" alt="">
+                </div>
+                <div class="card-body">
+                    <h3>Happy Days Garden</h3>
+                    <div>Procida, 80079</div>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos aliquam magnam tenetur, explicabo ipsa officia sed aliquid quia nihil qui provident dicta cupiditate inventore aspernatur, corrupti alias eligendi obcaecati id?</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card">
+                <div class="card-head">
+                    <img src="../images/prova.jpg" alt="">
+                </div>
+                <div class="card-body">
+                    <h3>Happy Days Garden</h3>
+                    <div>Procida, 80079</div>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos aliquam magnam tenetur, explicabo ipsa officia sed aliquid quia nihil qui provident dicta cupiditate inventore aspernatur, corrupti alias eligendi obcaecati id?</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card">
+                <div class="card-head">
+                    <img src="../images/prova.jpg" alt="">
+                </div>
+                <div class="card-body">
+                    <h3>Happy Days Garden</h3>
+                    <div>Procida, 80079</div>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos aliquam magnam tenetur, explicabo ipsa officia sed aliquid quia nihil qui provident dicta cupiditate inventore aspernatur, corrupti alias eligendi obcaecati id?</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card">
+                <div class="card-head">
+                    <img src="../images/prova.jpg" alt="">
+                </div>
+                <div class="card-body">
+                    <h3>Happy Days Garden</h3>
+                    <div>Procida, 80079</div>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos aliquam magnam tenetur, explicabo ipsa officia sed aliquid quia nihil qui provident dicta cupiditate inventore aspernatur, corrupti alias eligendi obcaecati id?</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card">
+                <div class="card-head">
+                    <img src="../images/prova.jpg" alt="">
+                </div>
+                <div class="card-body">
+                    <h3>Happy Days Garden</h3>
+                    <div>Procida, 80079</div>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eos aliquam magnam tenetur, explicabo ipsa officia sed aliquid quia nihil qui provident dicta cupiditate inventore aspernatur, corrupti alias eligendi obcaecati id?</p>
+                </div>
+            </div>
+        </div>
+        
+
+     
+     </div>
+
+     
+    
+</template>
+
+<script>
+
+export default {
+  name: 'Card'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+    @import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css');
+   
+   * {
+       padding: 0px;
+       margin: 0px;
+       box-sizing: border-card;
+   }
+
+    .container {
+        max-width: 992px;
+        margin: 0 auto;
+        display: flex;  
+        flex-wrap: wrap;
+    }
+
+   .col {
+       width: 33%;
+   }
+
+   .card {
+       border: 1px solid black;
+       margin: 10px;
+   }
+
+   .card-body {
+       padding: 10px;
+   }
+
+   img {
+       display: block;
+       width: 100%;
+       height: 250px;
+       object-fit: cover;
+       object-position: center;
+   }
+   
+
+</style>
--- resources/js/guest/pages/Home.vue
+++ resources/js/guest/pages/Home.vue
@@ -1,12 +1,19 @@
 <template>
     <div>
         <h2>Questa è l'homepage</h2>
+        <Card/>
     </div>
 </template>
 
 <script>
+
+import Card from '../components/Card.vue';
+
 export default {
-    name: 'Home'
+    name: 'Home',
+    components: {
+        Card
+    }
 }
 </script>
 
