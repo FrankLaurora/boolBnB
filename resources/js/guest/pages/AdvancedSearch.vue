dbdb089ce91ed23811370eacb113e78fe453ee9e
alessandroNessi <alessandronessi@msn.com>
2021-12-29 14:31:34 +0100

    aggiunta dinamicità nella ricerca da home

--- resources/js/guest/pages/AdvancedSearch.vue
+++ resources/js/guest/pages/AdvancedSearch.vue
@@ -4,7 +4,7 @@
             <div class="input_bar">
 
                 <!--DATALIST keyup non funziona -->
-                <input list="addresses" id="addressInput" style="width:500px" name="search" v-model="search" @keyup="fetchResults(search)" @keyup.enter.prevent="$emit('advancedSearch', search)" placeholder="Dove vuoi andare?">
+                <input list="addresses" id="addressInput" style="width:500px" name="search" v-model="search" @keyup="fetchResults(search)" @change="fetchResults(search)" placeholder="Dove vuoi andare?">
                 <datalist style="width:500px" id="addresses">
                     <option style="width:500px" v-for="(element, index) in searchResults[0]" :key="index" :value="element.address.freeformAddress"></option>
                 </datalist>
@@ -25,7 +25,7 @@
                 </div>
                 <!-- riscrivo la query ogni qualvolta clicco su mostra appartamenti -->
                 <router-link :to="{ name: 'search', params: { slug:query} }">
-                    <button @click="advancedSearch()" class="ms_btn_advance">Mostra appartamenti </button>
+                    <button @click="advancedSearch()" class="ms_btn_advance">Filtra appartamenti </button>
                 </router-link>
             </div>
             
@@ -58,18 +58,14 @@ export default {
             apartments : [],
             lastPage : null,
             searchResults: [],
-            rooms: null,
-            guests: null,
+            rooms: 1,
+            guests: 1,
             distance: null,
             lat: null,
             lon: null,
-            search: "",
+            search: this.$route.params.location,
             lastSearch: "",
             services: [],
-            geo: {
-                lat: null,
-                lon: null
-            },
             query:'',
             serviceFilter : [],
             noResults : false,
@@ -81,12 +77,17 @@ export default {
 
         //compilazione automatica della datalist su chiamata tomtom
         fetchResults(search) {
-            if(search != '') {
+            this.lat=undefined;
+            this.lon=undefined;
+            if(this.search!='') {
                 fetch('https://api.tomtom.com/search/2/geocode/'+ search +'.json?key=jXiFCoqvlFBNjmqBX4SuU1ehhUX1JF7t&language=it-IT')
                 .then(response => response.json())
                 .then(data =>{
                     this.searchResults = [];
                     this.searchResults.push(data.results);
+                    this.lat=this.searchResults[0][0].position.lat;
+                    this.lon=this.searchResults[0][0].position.lon;
+                        // this.query='&lat='+this.latitude+'&lon='+this.longitude;
                 })
             }
         },
@@ -192,40 +193,83 @@ export default {
                 .catch(error => {
                     console.log(error)
                 })
+        },
+
+        explodeSlug(){
+            let slug=this.$route.params.slug;
+            slug=slug.substr(1);
+            let exploded=slug.split('&');
+            exploded.forEach(element => {
+                element=element.split('=');
+                switch (element[0]) {
+                    case 'lon':
+                        this.lon=parseFloat(element[1]);
+                    break;
+                    case 'lat':
+                        this.lat=parseFloat(element[1]);
+                    break;
+                    case 'dist':
+                        this.distance=parseInt(element[1]);
+                    break;
+                    case 'rooms':
+                        this.rooms=parseInt(element[1]);
+                    break;
+                    case 'guests':
+                        this.guests=parseInt(element[1]);
+                    break;
+                    case 'services':
+                        this.serviceFilter=[];
+                        let services=element[1].split('-');
+                        services.forEach(service=>{
+                            this.serviceFilter.push(parseInt(service));
+                        });
+                    break;
+                }
+            });
+        },
+
+        //mounts the query
+        mountSlug(){
+            this.query='';
+            this.query=`&lat=${this.lat}&lon=${this.lon}`
+            // ${this.distance ? '&dist=' + this.distance : ''}${this.rooms ? '&rooms=' + this.rooms : ''}${this.guests ? '&guests=' + this.guests : ''}${this.servicesId != "" ? '&services=' + servicesId : ''}/?page=${this.page}`;
+            this.$rounte.params.slug=this.query;
+            console.log(this.$route.params.slug);
+            console.log(this.query);
+            alert('uscito');
+        },
+
+        //get apartments function
+        getApartments(){
+            axios.get(`http://localhost:8000/api/apartments/search/${this.$route.params.slug}`)
+            .then(response => {
+                this.apartments = [];
+                this.apartments = response.data.data.data;
+                this.noResults = false;
+                if(this.apartments.length < 1){
+                    this.noResults = true;
+                }else{
+                    if(this.apartments[0].city!=undefined){
+                        this.search=this.apartments[0].city;
+                    }
+                }
+                this.lastPage = response.data.data.last_page;
+            })
+            .catch(error => {
+                console.log(error)
+            })
+            axios.get(`/api/services`)
+                .then(response =>{
+                this.services = response.data.data;
+            });
+            window.scrollTo(0, 0);
         }
     },
-
+    //on mounted i get parameters passed from home and get apartments
     created() {
-            //???????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????????
-            // fetch('https://api.tomtom.com/search/2/geocode/'+ this.$route.params.slug +'.json?key=jXiFCoqvlFBNjmqBX4SuU1ehhUX1JF7t&language=it-IT')
-            // .then(response => response.json())
-            // .then(data=>{
-            //     this.geo.lat=data.results[0].position.lat;
-            //     this.geo.lon=data.results[0].position.lon;
-        this.lat=this.$route.params.lat;
-        this.lon=this.$route.params.lon;
-        this.query=this.$route.params.slug;
-        axios.get(`http://localhost:8000/api/apartments/search/${this.query}&dist=25`)
-        .then(response => {
-            this.apartments = [];
-            this.apartments = response.data.data.data;
-            this.noResults = false;
-            if(this.apartments.length < 1){
-                this.noResults = true;
-            }
-            this.lastPage = response.data.data.last_page;
-        })
-        .catch(error => {
-            console.log(error)
-        })
-        // });
-
-        axios.get(`/api/services`)
-            .then(response =>{
-            this.services = response.data.data;
-        });
-        window.scrollTo(0, 0);
-    }
+        this.explodeSlug();
+        this.getApartments();
+    },
 }
 </script>
 
